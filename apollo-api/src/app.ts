d2a56f952aceb6769d78eb5ed33b7468e672cefd
CarolynCCCC <126298896+CarolynCCCC@users.noreply.github.com>
2026-02-01 10:52:42 +0800

    Update app.ts

--- apollo-api/src/app.ts
+++ apollo-api/src/app.ts
@@ -4,10 +4,12 @@ import playerRoutes from './routes/players';
 import gameRoutes from './routes/game';
 import { errorHandler } from './middleware/errorHandler';
 import { API_BASE_PATH } from './constant/api';
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(API_BASE_PATH, roomRoutes);
 app.use(API_BASE_PATH, playerRoutes);
